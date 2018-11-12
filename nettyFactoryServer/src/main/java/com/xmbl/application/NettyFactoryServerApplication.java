18aaa2d474fe85409fdb5f6eb8e1a6e892522884
Noseparte <noseparte@aliyun.com>
2018-11-12 16:00:03 +0800

    netty
    Netty base frame construction.

---
+++ nettyFactoryServer/src/main/java/com/xmbl/application/NettyFactoryServerApplication.java
@@ -0,0 +1,12 @@
+package com.xmbl.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyFactoryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyFactoryServerApplication.class, args);
+	}
+}
