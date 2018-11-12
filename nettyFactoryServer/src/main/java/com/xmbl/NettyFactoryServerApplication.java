209d861fe966812d0d1d7489ef64ccc7a90a42e7
Noseparte <noseparte@aliyun.com>
2018-11-12 18:57:34 +0800

    Netty websocket 时间服务器

--- nettyFactoryServer/src/main/java/com/xmbl/NettyFactoryServerApplication.java
+++ nettyFactoryServer/src/main/java/com/xmbl/NettyFactoryServerApplication.java
@@ -1,4 +1,4 @@
-package com.xmbl.application;
+package com.xmbl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,4 +9,5 @@ public class NettyFactoryServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NettyFactoryServerApplication.class, args);
 	}
+
 }
